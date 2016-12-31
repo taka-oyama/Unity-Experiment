699d90ce6d595c45090f5efe2b885ff4d68523a6
Taka Oyama <taka.oyama@gmail.com>
2016-12-31 18:28:35 +0900

    namespace

--- Assets/Plugins/Extensions/ActionExtension.cs
+++ Assets/Plugins/Extensions/ActionExtension.cs
@@ -1,28 +1,29 @@
-﻿using System;
-
-public static class ActionExtension
+﻿namespace System
 {
-	public static void SafeInvoke(this Action action)
+	public static class ActionExtension
 	{
-		if(action == null) return;
-		action();
-	}
+		public static void SafeInvoke(this Action action)
+		{
+			if(action == null) return;
+			action();
+		}
 
-	public static void SafeInvoke<T>(this Action<T> action, T arg)
-	{
-		if(action == null) return;
-		action(arg);
-	}
+		public static void SafeInvoke<T>(this Action<T> action, T arg)
+		{
+			if(action == null) return;
+			action(arg);
+		}
 
-	public static void SafeInvoke<T1, T2>(this Action<T1, T2> action, T1 arg1, T2 arg2)
-	{
-		if(action == null) return;
-		action(arg1, arg2);
-	}
+		public static void SafeInvoke<T1, T2>(this Action<T1, T2> action, T1 arg1, T2 arg2)
+		{
+			if(action == null) return;
+			action(arg1, arg2);
+		}
 
-	public static void SafeInvoke<T1, T2, T3>(this Action<T1, T2, T3> action, T1 arg1, T2 arg2, T3 arg3)
-	{
-		if(action == null) return;
-		action(arg1, arg2, arg3);
+		public static void SafeInvoke<T1, T2, T3>(this Action<T1, T2, T3> action, T1 arg1, T2 arg2, T3 arg3)
+		{
+			if(action == null) return;
+			action(arg1, arg2, arg3);
+		}
 	}
 }
