d5409851a5c57b5f3eaddedc7ef4999f23119229
Taka Oyama <taka.oyama@gmail.com>
2016-12-31 20:23:30 +0900

    string extension

---
+++ Assets/Plugins/Extensions/Global/StringExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class StringExtension
+{
+	/// <summary>
+	/// Converts string to Enum.
+	/// </summary>
+	/// <typeparam name="T">Type of enum</typeparam>
+	public static T ToEnum<T>(this string source, bool ignoreCase = true) where T : struct
+	{
+		Type type = typeof(T);
+		if(!type.IsEnum) {
+			string message = string.Format("<{0}> is not an Enum Type", type.Name);
+			throw new ArgumentException(message);
+		}
+		return (T)Enum.Parse(type, source, ignoreCase);
+	}
+
+	/// <summary>
+	/// Extension method for applying string.Format on an instance.
+	/// </summary>
+	public static string FormatWith(this string value, params object[] args)
+	{
+		return string.Format(value, args);
+	}
+}
