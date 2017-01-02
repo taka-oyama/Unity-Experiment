a6c9934c20a9ef937b9e01abb9de2b31aaf01eb7
Taka Oyama <taka.oyama@gmail.com>
2017-01-03 01:42:22 +0900

    Move files around

---
+++ Assets/Standard Assets/Editor/AssetBundle/AssetBundleBuilder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class AssetBundleBuilder
+{
+	[MenuItem("File/AssetBundle/Build", true, 1)]
+	static void Build()
+	{
+		BuildTarget buildTarget = EditorUserBuildSettings.activeBuildTarget;
+		string outputPath = string.Join("/", new [] { Application.dataPath, "AssetBundles", buildTarget.ToString() });
+		BuildAssetBundleOptions options = BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.StrictMode;
+		Directory.CreateDirectory(outputPath);
+		BuildPipeline.BuildAssetBundles(outputPath, options, buildTarget);
+	}
+}
---
+++ Assets/Standard Assets/Editor/Support/Windows/DiagnosticsEditorWindow.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DiagnosticsEditorWindow : EditorWindow
+{
+	string boxMessage;
+
+	[MenuItem("Window/Diagnostics")]
+	public static void Window()
+	{
+		GetWindow<DiagnosticsEditorWindow>("Diagnostics"); 
+	} 
+
+	protected virtual void OnGUI()
+	{
+		EditorGUILayout.HelpBox(boxMessage, MessageType.Info, true);
+	}
+
+	protected virtual void OnRecompile(double duration)
+	{
+		boxMessage = string.Format("Script Compilation: {0}s", duration.ToString("0.00"));
+		Repaint();
+	}
+
+	protected virtual void OnInspectorUpdate()
+	{ 
+		DetectRecompile();
+	}
+
+	#region Compilation Time
+	bool isTrackingTime;
+	double startTime, finishTime;
+
+	void DetectRecompile()
+	{
+		if (EditorApplication.isCompiling && !isTrackingTime) {
+			startTime = EditorApplication.timeSinceStartup; 
+			isTrackingTime = true;
+		} else if (!EditorApplication.isCompiling && isTrackingTime) {
+			finishTime = EditorApplication.timeSinceStartup; 
+			isTrackingTime = false;
+			OnRecompile(finishTime - startTime);
+		}
+	}
+	#endregion
+}
