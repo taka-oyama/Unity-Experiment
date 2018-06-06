d7b3a2ef51a09b8544ef5e32118518092a317216
Taka Oyama <taka.oyama@gmail.com>
2018-06-07 01:02:19 +0900

    more cleanup

--- Assets/Game/Shared/GameObjects/Character.cs
+++ Assets/Game/Shared/GameObjects/Character.cs
@@ -9,16 +9,16 @@ public partial class Character : MonoBehaviour
 	[Inject]
 	public CharacterData Data;
 
-	public Character Father { get; protected set; }
-	public Character Mother { get; protected set; }
-	public Character Spouse { get; protected set; }
-	public List<Character> Children = new List<Character>();
-
 	public Guid Id => Data.Id;
 	public string Name => Data.Name;
 	public Gender Gender => Data.Gender;
 	public DateTime Birthday => Data.Birthday;
 	public DateTime? Deathday => Data.Deathday;
+
+	public Character Father { get; protected set; }
+	public Character Mother { get; protected set; }
+	public Character Spouse { get; protected set; }
+	public List<Character> Children = new List<Character>();
 
 	public int Age => (cachedAge = cachedAge ?? (int)CalculateAge()).Value;
 	public bool IsMale => Gender == Gender.Male;
@@ -30,6 +30,7 @@ public partial class Character : MonoBehaviour
 	public bool IsMarried => !IsSingle;
 	public bool CanReproduce => IsFemale && IsMarried && !IsPregnant;
 	public bool IsPregnant => Children.Any(c => c.Age < 0);
+	public bool IsDead => Deathday.HasValue;
 
 	int? cachedAge;
 
@@ -91,15 +92,12 @@ public partial class Character : MonoBehaviour
 			personality: Data.Personality + Spouse.Data.Personality * UnityEngine.Random.Range(-1.25f, 1.25f) / 2f,
 			baseSpec: Data.BaseSpec + Spouse.Data.BaseSpec * UnityEngine.Random.Range(-1.25f, 1.25f) / 2f
 		);
+		Data.ChildrenIds.Add(childData.Id);
 
 		Character child = factory.Create(childData);
 		child.Mother = this;
-		child.Data.MotherId = Data.Id;
-
 		child.Father = Mother.Spouse;
-		child.Data.FatherId = Spouse.Data.Id;
-
-		Data.ChildrenIds.Add(child.Id);
+		Children.Add(child);
 
 		return child;
 	}
